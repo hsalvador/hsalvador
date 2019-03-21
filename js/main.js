ceb4220aacb0ba51360f470a4048baa06b97c0c0
Henrique Salvador <henrique.r.salvador@gmail.com>
2019-03-21 09:33:27 -0400

    Adding Fancybox lib for photos

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$('[data-fancybox="ideation-gallery"]').fancybox({
+  buttons: [
+      "zoom",
+      "close"
+    ],
+});
+
+
+$('[data-fancybox="breakpoints-gallery"]').fancybox({
+  buttons: [
+      "zoom",
+      "close"
+    ],
+});
