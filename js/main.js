0b39832a5e7f311ede1a23f9435351ed73443e9d
Henrique Salvador <henrique.r.salvador@gmail.com>
2019-03-25 19:30:31 -0400

    Adding Nature's Bystanders content

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// TODO - this looks ugly, gotta refactor
+
 $('[data-fancybox="ideation-gallery"]').fancybox({
   buttons: [
       "zoom",
@@ -14,6 +16,20 @@ $('[data-fancybox="breakpoints-gallery"]').fancybox({
 });
 
 $('[data-fancybox="survey-gallery"]').fancybox({
+  buttons: [
+      "zoom",
+      "close"
+    ],
+});
+
+$('[data-fancybox="define-gallery"]').fancybox({
+  buttons: [
+      "zoom",
+      "close"
+    ],
+});
+
+$('[data-fancybox="prototype-2-gallery"]').fancybox({
   buttons: [
       "zoom",
       "close"
